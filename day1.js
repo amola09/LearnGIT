9102ed7a617227518c88d5dbd33fb302daa22b6a
amola09 <Amol7Anpat@gmail.com>
2021-08-16 12:11:46 +0530

    adding day1.js

---
+++ day1.js
@@ -0,0 +1,42 @@
+let c = 10
+console.log(c)
+console.log (typeof c)
+
+let firstName = "Amol"
+console.log (firstName)
+console.log(typeof firstName)
+console.log("-----------------------")
+
+// Number ,String ,Boolean ,Array ,Objects
+// arithmatic operators
+
+let a=20
+let b=30
+
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+
+console.log("-----------------------------------------")
+
+// Function 
+//function declartion
+// keyword   functionname  paramters
+
+function cal(a,b){   // blocks 
+    console.log(a+b)
+    console.log(a-b)
+    console.log(a*b)
+    console.log(a/b)  // statements
+}
+
+cal(50,25)   // function call  (actual values ) arguments 
+cal(20,10)
+
+
+
+
+
+
